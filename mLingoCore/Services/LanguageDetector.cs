f43f8374f06749996b8a6f90d1fe766eed0a3712
Michal <mszostakk01@gmail.com>
2019-12-01 12:42:44 +0100

    add core project files

---
+++ mLingoCore/Services/LanguageDetector.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using NTextCat;
+
+namespace mLingoCore.Services
+{
+    class LanguageDetector : ILanguageDetector
+    {
+        private readonly RankedLanguageIdentifier mIdentifier;
+        public LanguageDetector()
+        {
+            var factory = new RankedLanguageIdentifierFactory();
+            mIdentifier = factory.Load(@".\Assets\LanguageDetectorProfiles\Core14.profile.xml");
+        }
+
+        public string DetectLanguage(string input)
+        {
+            var languages = mIdentifier.Identify(input);
+            var mostCertainLanguage = languages.FirstOrDefault();
+            return mostCertainLanguage != null ? mostCertainLanguage.Item1.Iso639_2T : "unknown";
+        }
+    }
+}
