39e5334208149899968c7d9bc6020a2b718cb3dc
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2019-12-02 17:36:48 +0100

    Footer fix

--- mLingo/ClientApp/src/components/MainPageComponents/Footer.js
+++ mLingo/ClientApp/src/components/MainPageComponents/Footer.js
@@ -5,11 +5,9 @@ const Footer = props => {
   return (
     <div className="footer">
       <div className="social">Facebook-monkey</div>
-      <div className="social">Instagram-monkey</div>
       <div className="social">Github-monkey</div>
       <div className="social">Slack-monkey</div>
       <div className="social">Mail-monkey</div>
-      <div className="social">Twitter-monkey</div>
       <div className="footer-end-text">
         <p>mLingo Team 2019 </p>
       </div>
