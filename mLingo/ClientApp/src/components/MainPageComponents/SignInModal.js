74f749bca4212011aa72fe418375399b463ce62b
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2019-12-02 20:56:36 +0100

    Add modal to the sign in button

--- mLingo/ClientApp/src/components/MainPageComponents/SignInModal.js
+++ mLingo/ClientApp/src/components/MainPageComponents/SignInModal.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import Register from "./Register";
 
 const SignInModal = props => {
-  const { buttonLabel, className } = props;
-
   const [modal, setModal] = useState(false);
 
   const toggle = () => setModal(!modal);
@@ -14,21 +13,12 @@ const SignInModal = props => {
         Sign in
       </button>
       <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalHeader toggle={toggle}>SIGN IN</ModalHeader>
         <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <Register />
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={toggle}>
-            Do Something
-          </Button>{" "}
-          <Button color="secondary" onClick={toggle}>
+          <Button color="danger" onClick={toggle}>
             Cancel
           </Button>
         </ModalFooter>
