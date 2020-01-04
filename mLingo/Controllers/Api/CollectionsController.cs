1ab204bbc4541ba128bc0b537faa3bb9bd45d9a3
Michał Szóstak <mszostakk01@gmail.com>
2020-01-04 19:19:31 +0100

    setup collections controller outline

---
+++ mLingo/Controllers/Api/CollectionsController.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mLingo.Extensions.Authentication;
+
+namespace mLingo.Controllers.Api
+{
+    [AuthorizeToken]
+    public class CollectionsController : Controller
+    {
+        [HttpGet]
+        public Task<IActionResult> GetCollection([FromQuery] string id)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public Task<IActionResult> GetUserCollections([FromQuery] string username)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public Task<IActionResult> CreateCollection([FromBody] object newCollectionData)
+        {
+            return null;
+        }
+
+        [HttpPut]
+        public Task<IActionResult> UpdateCollection([FromQuery] string id)
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        public Task<IActionResult> DeleteCollection([FromQuery] string id)
+        {
+            return null;
+        }
+
+    }
+}
