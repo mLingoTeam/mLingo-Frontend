7ea42cbf7c0bb3a5d6be88b560e37ff139206eb9
Michał Szóstak <mszostakk01@gmail.com>
2020-01-04 19:30:48 +0100

    setup di for collections  controller

--- mLingo/Controllers/Api/CollectionsController.cs
+++ mLingo/Controllers/Api/CollectionsController.cs
@@ -1,16 +1,42 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using mLingo.Extensions.Authentication;
+using mLingo.Models.Database;
 
 namespace mLingo.Controllers.Api
 {
     [AuthorizeToken]
     public class CollectionsController : Controller
     {
+        #region PrivateFields
+
+        private readonly ILogger _apiLogger;
+
+        private readonly IConfiguration _apiConfiguration;
+
+        private readonly AppDbContext _apiDbContext;
+
+        #endregion
+
+
+        #region Constructor
+
+        public CollectionsController(ILogger logger, IConfiguration configuration, AppDbContext appDbContext)
+        {
+            _apiLogger = logger;
+            _apiConfiguration = configuration;
+            _apiDbContext = appDbContext;
+        }
+
+        #endregion
+
+
         [HttpGet]
         public Task<IActionResult> GetCollection([FromQuery] string id)
         {
-            return null;
+            
         }
 
         [HttpGet]
---
+++ mLingoCore/Models/Api/ResponseModels/CollectionResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using mLingoCore.Models.FlashCards;
+
+namespace mLingoCore.Models.Api.ResponseModels
+{
+    class CollectionResponse : CollectionData
+    {
+    }
+}
