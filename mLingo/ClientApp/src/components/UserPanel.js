b94602c08e629fa89b93033c72db3e2f1db217fd
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-02-10 21:39:41 +0100

    order stuff

--- mLingo/ClientApp/src/components/UserPanel.js
+++ mLingo/ClientApp/src/components/UserPanel.js
@@ -11,7 +11,7 @@ class UserPanel extends React.Component {
       this.props.history.push("/");
     }
 
-    this.state = { fields: [], exist: null, request: "example1" }
+    this.state = { fields: [], exist: null, type: 'username', request: "example1" }
 
     this.findcollection = this.findcollection.bind(this);
     this.changeRequest = this.changeRequest.bind(this);
@@ -20,9 +20,13 @@ class UserPanel extends React.Component {
   changeRequest(event) {
     this.setState({ "request": event.target.value });
   }
+  changeRequestType(event) {
+    console.log(event)
+  }
+
 
   async findcollection() {
-    const collectiondata = await authenticationService.requestCollection(this.state.request);
+    const collectiondata = await authenticationService.requestCollection(this.state.type, this.state.request);
     console.log(collectiondata);
     if (collectiondata.Successful === true) {
       this.setState({ "fields": collectiondata.Response, "exist": true });
@@ -47,6 +51,10 @@ class UserPanel extends React.Component {
             <h1>Hi {localStorage.getItem("currentUser")}!</h1>
             <h3>Welcome in MLingo!</h3>
             <p>Search collection by user or by name!</p>
+            <div>
+              <div><input type="radio" name="radAnswer" value="username" checked onChange={this.changeRequestType} /> <label>Search by username</label></div>
+              <div><input type="radio" name="radAnswer" value="name" onChange={this.changeRequestType} /> <label>Search by set name</label></div>
+            </div>
             <input className="searchcollectioninput" type="text" onChange={this.changeRequest} />
             <button onClick={this.findcollection}>Find</button>
             <button onClick={this.logout}>Logout</button>
