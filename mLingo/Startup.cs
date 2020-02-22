039e8fd893e4ad88ac885b5515c8068bcc1b9c82
Michał Szóstak <mszostakk01@gmail.com>
2020-02-23 00:03:20 +0100

    add account manager to DI

--- mLingo/Startup.cs
+++ mLingo/Startup.cs
@@ -14,6 +14,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using mLingo.Models.Database;
 using mLingo.Models.Database.User;
+using mLingo.Modules.Implementations;
+using mLingo.Modules.Interfaces;
 
 namespace mLingo
 {
@@ -100,6 +102,8 @@ namespace mLingo
             });
 
             services.AddControllersWithViews();
+
+            services.AddTransient<IAccountManager, StandardAccountManager>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
