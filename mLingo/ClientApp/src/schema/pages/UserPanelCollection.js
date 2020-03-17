1e846d80ce48a996b011784ce0833eb6eac8a218
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-03-17 20:51:08 +0100

    if user is logged show menu in search flashcards

--- mLingo/ClientApp/src/schema/pages/UserPanelCollection.js
+++ mLingo/ClientApp/src/schema/pages/UserPanelCollection.js
@@ -1,15 +1,25 @@
 import React from "react";
 import Layout from '../layouts/Layout'
+import UserLayout from '../layouts/UserLayout'
 import Collection from '../components/CardComponents/Collection'
 
 class UserPanelCollection extends React.Component {
 
-
+    constructor() {
+        super();
+        this.state = localStorage.getItem("currentUser") ? true : false;
+    }
     render() {
         return (
-            <Layout>
-                <Collection />
-            </Layout>
+
+            this.state ?
+                <UserLayout>
+                    <Collection />
+                </UserLayout> :
+                <Layout>
+                    <Collection />
+                </Layout>
+
         )
     }
 }
