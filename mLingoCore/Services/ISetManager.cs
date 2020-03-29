58ca29292e842c9371d5b628d0fd16a894ca0872
Michał Szóstak <mszostakk01@gmail.com>
2020-03-29 21:23:39 +0200

    add an interface for set management

---
+++ mLingoCore/Services/ISetManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using mLingoCore.Models.Api.Base;
+
+namespace mLingoCore.Services
+{
+    /// <summary>
+    /// Base definition of set manager functionality
+    /// </summary>
+    public interface ISetManager
+    {
+        KeyValuePair<ApiResponse, int> Find(string id, string name);
+
+        KeyValuePair<ApiResponse, int> UserSets(string username);
+
+        KeyValuePair<ApiResponse, int> CreateSet(string username, object newSetData);
+
+        KeyValuePair<ApiResponse, int> DeleteSet(string id);
+
+        KeyValuePair<ApiResponse, int> EditSet(string id, object editedData);
+
+        KeyValuePair<ApiResponse, int> Add(string setId, string collectionId);
+
+        KeyValuePair<ApiResponse, int> Remove(string setId, string collectionId);
+    }
+}
