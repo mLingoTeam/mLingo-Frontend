bb8f8c95fd2b2426fee8f78ff3271ff494a0777c
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-04-07 19:37:00 +0200

    Add loading animation

---
+++ mLingo/ClientApp/src/schema/components/Animations/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Loading = ( props ) => {
+    return(
+        <div className="loader introLoader">
+            <span className="introSpan "></span>
+            <span className="introSpan"></span>
+            <span className="introSpan"></span>
+            <span className="introSpan"></span>
+        </div>
+    )
+}
+
+export default Loading;
--- mLingo/ClientApp/src/schema/components/LandPageComponents/Step.js
+++ mLingo/ClientApp/src/schema/components/LandPageComponents/Step.js
@@ -5,7 +5,7 @@ const Step = ( {props}) => {
 
     return(
         <div className="Step" key={props.title}>
-            <img src={props.img} className="img-fluid" data-aos="flip-left"/>
+            <img src={props.img} className="img-fluid" data-aos="flip-left"  data-aos-delay="200" data-aos-duration="1000"/>
             <div>
                 <h2>{props.title}</h2>
                 <p>{props.desc}</p>
