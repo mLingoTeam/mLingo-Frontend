c24b0609f805c9a03c68bd06e24aed78508abe47
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-04-07 16:42:45 +0200

    implement welcome section mobile look

--- mLingo/ClientApp/src/schema/components/LandPageComponents/WelcomeSection.js
+++ mLingo/ClientApp/src/schema/components/LandPageComponents/WelcomeSection.js
@@ -1,8 +1,10 @@
 import React from "react";
+import backgroundimg from '../../../img/landpage1.png'
 
 const WelcomeSection = props => {
     return (
         <div className="welcome-section">
+            <img src={backgroundimg} className="img-fluid"/>
             <div>
                 <h2>mLingo</h2>
                 <p> practice your skills daily through easy to set up learning plans. Join now! </p>
