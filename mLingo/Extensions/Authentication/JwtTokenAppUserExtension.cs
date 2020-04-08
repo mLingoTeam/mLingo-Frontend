40323a22903a309f150ef86a8acfebbf17c42710
Michał Szóstak <mszostakk01@gmail.com>
2020-04-08 19:14:11 +0200

    Refactor jwt handler to add admin account

--- mLingo/Extensions/Authentication/JwtTokenAppUserExtension.cs
+++ mLingo/Extensions/Authentication/JwtTokenAppUserExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity.Core.Metadata.Edm;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -20,8 +21,9 @@ namespace mLingo.Extensions.Authentication
         /// <returns></returns>
         public static string GenerateJwtToken(this AppUser user, IConfiguration configuration)
         {
-            // Set our tokens claims
-            var claims = new[]
+            
+            // Set our tokens claims for member
+            var memberClaims = new[]
             {
                 // Unique ID for this token
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
@@ -31,8 +33,19 @@ namespace mLingo.Extensions.Authentication
                 new Claim(ClaimsIdentity.DefaultNameClaimType, user.UserName),
 
                 // Add user Id so that UserManager.GetUserAsync can find the user based on Id
-                new Claim(ClaimTypes.NameIdentifier, user.Id)
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                
             };
+
+            // Set token claims for admin
+            var adminClaims = new[]
+            {
+                new Claim(ClaimTypes.Role, "admin")
+            };
+            Array.Copy(memberClaims, adminClaims, memberClaims.Length);
+
+
+            var isAdmin = user.Email.Equals("admin@mlingo.com");
 
             // Create the credentials used to generate the token
             var credentials = new SigningCredentials(
@@ -45,10 +58,9 @@ namespace mLingo.Extensions.Authentication
             var token = new JwtSecurityToken(
                 issuer: configuration["Jwt:JwtIssuer"],
                 audience: configuration["Jwt:JwtAudience"],
-                claims: claims,
+                claims: isAdmin ? adminClaims : memberClaims,
                 signingCredentials: credentials,
-                // Expire if not used for 3 months
-                expires: DateTime.Now.AddMonths(3)
+                expires: DateTime.Now.AddHours(3)
             );
 
             // Return the generated token
