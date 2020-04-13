87f9f784948b6b09069d3881ac9f31952b214635
Michał Szóstak <mszostakk01@gmail.com>
2020-04-13 13:51:55 +0200

    Add standard newsletter manager skeleton with dependency injection

---
+++ mLingo/Controllers/Api/NewsletterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using mLingo.Models.Database;
+using mLingo.Modules;
+using mLingoCore.Models.Forms.Newsletter;
+using mLingoCore.Services;
+
+namespace mLingo.Controllers.Api
+{
+    public class NewsletterController : Controller
+    {
+        public NewsletterController(INewsletterManager manager, AppDbContext dbContext)
+        {
+            
+        }
+
+        public IActionResult SignUp([FromBody] RegisterForNewsletterForm form)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ mLingo/Modules/StandardNewsletterManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using mLingoCore.Models.Api.Base;
+using mLingoCore.Models.Forms.Newsletter;
+using mLingoCore.Services;
+
+namespace mLingo.Modules
+{
+    public class StandardNewsletterManager : INewsletterManager
+    {
+        public ApiResponse SignUp(RegisterForNewsletterForm form)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
