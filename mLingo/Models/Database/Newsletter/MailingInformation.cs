ce4d5fc52f56b4b8f57caec38ecfcb2a58a3ffe1
Michał Szóstak <mszostakk01@gmail.com>
2020-04-13 13:45:25 +0200

    Update database context to store mailing information

---
+++ mLingo/Models/Database/Newsletter/MailingInformation.cs
@@ -0,0 +1,9 @@
+﻿namespace mLingo.Models.Database.Newsletter
+{
+    public class MailingInformation
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+    }
+}
