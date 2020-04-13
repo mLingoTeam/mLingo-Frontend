24639fe1ff95c421bfd519338a19fe08fea31d28
Michał Szóstak <mszostakk01@gmail.com>
2020-04-13 14:02:41 +0200

    Add signup implementation and endpoint documentation

--- mLingo/Modules/StandardNewsletterManager.cs
+++ mLingo/Modules/StandardNewsletterManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using mLingo.Models.Database;
+using mLingo.Models.Database.Newsletter;
+using mLingoCore.Models.Api;
 using mLingoCore.Models.Api.Base;
 using mLingoCore.Models.Forms.Newsletter;
 using mLingoCore.Services;
@@ -12,7 +14,22 @@ namespace mLingo.Modules
 
         public ApiResponse SignUp(RegisterForNewsletterForm form)
         {
-            
+            try
+            {
+                var mailingInfo = new MailingInformation
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Email = form.Email
+                };
+
+                DbContext.MailingList.Add(mailingInfo);
+                DbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return ApiResponse.ServerExceptionResponse(ErrorMessages.Server.ActionFail("sign up for newsletter"), e.StackTrace, 500);
+            }
+            return ApiResponse.StatusCodeResponse(200);
         }
     }
 }
