6ac582c32ac5c06abab0097f6f7d485e80eb59f1
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-04-17 10:19:33 +0200

    CollectionCreateDetails BEM refactor

--- mLingo/ClientApp/src/schema/components/account_/collection_/CollectionCreate/CollectionView/CollectionCreateDetails.js
+++ mLingo/ClientApp/src/schema/components/account_/collection_/CollectionCreate/CollectionView/CollectionCreateDetails.js
@@ -1,27 +1,21 @@
 import React from 'react'
-import {
- CardBody, CardTitle } from 'reactstrap';
 import Square from '../../../../../../img/collection.svg';
 
 const CollectionCreateDetails = ( { state, functions } ) => {
 
     return (
-            <div className="collection__details createCollection">
-                <div>
-                    <CardBody className="d-flex flex-nowrap">
-                        <div className="flashsquare"><img src={Square} /></div>
-                        <div>
-                            <CardTitle> Title </CardTitle>
-                            <input name="collectionTitle" type="text" size="45" placeholder="Type here" value={state.collectionTitle} onChange={functions.handleChange} required />
-                            <CardTitle> Description </CardTitle>
-                            <textarea name="collectionDescription"  rows="5" cols="45" placeholder="Type here" onChange={functions.handleChange} value={state.collectionDescription} required />
-                            <div>
-                                <button onClick={state.functionii} class="green-button"> { state.edit ? "edit collection" : "create collection" } </button>
-                                <button className="green-button"> add to the study set </button>
-                            </div>
+            <div className="create__details">
+                    <img className="details__img" src={Square} />
+                    <div className="details__body">
+                        <div className="details__title"> title </div>
+                        <input name="collectionTitle" className="details__description" type="text" size="46" placeholder="Type here" value={state.collectionTitle} onChange={functions.handleChange} required />
+                        <div className="details__title"> description </div>
+                        <textarea name="collectionDescription"  className="details__description" rows="5" cols="45" placeholder="Type here" onChange={functions.handleChange} value={state.collectionDescription} required />
+                        <div className="details__buttons">
+                            <button onClick={state.functionii} class="green-button"> { state.edit ? "edit collection" : "create collection" } </button>
+                            <button className="green-button"> add to the study set </button>
                         </div>
-                    </CardBody>
-                </div>
+                    </div>
             </div >
     )
 
