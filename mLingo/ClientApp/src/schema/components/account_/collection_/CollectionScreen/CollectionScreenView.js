3727c1996fb2cf05a220a0bd08089cb45e54c08f
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-04-17 14:06:06 +0200

    Rebuild CollectionScreen

--- mLingo/ClientApp/src/schema/components/account_/collection_/CollectionScreen/CollectionScreenView.js
+++ mLingo/ClientApp/src/schema/components/account_/collection_/CollectionScreen/CollectionScreenView.js
@@ -21,9 +21,7 @@ const CollectionSectionView = ( { state, functions } ) => {
 
                 </div> : <h1>Collection removed!</h1> : <h1>loading</h1>
             }
-            <div>
                 <Link to='/head' className="green--button">go back</Link>
-            </div>
             {
                 state.collection ? <div className="flex--around">
                     <button className="green--button" onClick={functions.removeCollection}>Remove Collection</button>
