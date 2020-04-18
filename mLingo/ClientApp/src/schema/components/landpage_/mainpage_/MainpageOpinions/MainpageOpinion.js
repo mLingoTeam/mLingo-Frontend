c643508b27fae0af84d933e8ebf2205b722f6c47
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-04-18 12:56:32 +0200

    rebuild mainpage opinion BEM

--- mLingo/ClientApp/src/schema/components/landpage_/mainpage_/MainpageOpinions/MainpageOpinion.js
+++ mLingo/ClientApp/src/schema/components/landpage_/mainpage_/MainpageOpinions/MainpageOpinion.js
@@ -4,9 +4,9 @@ const MainpageOpinion = ( {set, index}) => {
 
 
     return(
-        <div className="Opinion offset-md-3  col-md-8" key={index}>
-            <img src={set.photo} />
-            <p className="col-md-7">{set.opinion}</p>
+        <div className="opinion__container" key={index}>
+            <img className="opinion__img" src={set.photo} />
+            <div className="opinion__description">{set.opinion}</div>
         </div>
     )
 }
