97fca6c28491d9372296aef00f01c2901b4bc1e8
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-04-18 14:10:39 +0200

    rebuild mainpage welcome

--- mLingo/ClientApp/src/schema/components/landpage_/mainpage_/MainpageWelcome/MainpageWelcome.js
+++ mLingo/ClientApp/src/schema/components/landpage_/mainpage_/MainpageWelcome/MainpageWelcome.js
@@ -3,12 +3,12 @@ import backgroundimg from '../../../../../img/landpage1.png'
 
 const MainpageWelcome = props => {
     return (
-        <div className="welcome-section">
-            <img src={backgroundimg} className="img-fluid"/>
-            <div>
-                <h2 className="main-page-h2">mLingo</h2>
-                <p> practice your skills daily through easy to set up learning plans. Join now! </p>
-                <button className="green-button px-5">get started</button>
+        <div className="welcome__container">
+            <img src={backgroundimg} className="welcome__img"/>
+            <div className="welcome__details">
+                <div className="welcome__title">mLingo</div>
+                <p className="welcome__description"> practice your skills daily through easy to set up learning plans. Join now! </p>
+                <button className="green--button">get started</button>
             </div>
         </div>
     );
