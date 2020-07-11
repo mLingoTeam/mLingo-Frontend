8017cc09a7ddafb04173ee5df3a07cfb7cac5d31
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-07-11 17:56:59 +0200

    add loading component html structure

---
+++ mLingo/ClientApp/src/schema/components/loading/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Loading(props){
+    return(
+        <div class="container">
+            <div class="📦"></div>
+            <div class="📦"></div>
+            <div class="📦"></div>
+            <div class="📦"></div>
+            <div class="📦"></div>
+        </div>
+    )
+}
