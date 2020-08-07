7599f3750269f0911146d8e367d7f95465dc80bb
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-08-07 13:38:10 +0200

    add set form sketch

--- mLingo/ClientApp/src/schema/components/account_/set_/SetScreen/SetScreenView.js
+++ mLingo/ClientApp/src/schema/components/account_/set_/SetScreen/SetScreenView.js
@@ -7,7 +7,19 @@ const SetScreenView = ( { state, functions } ) => {
     return (
         <div className="set__container">
             <h2 className="set__title">{state.name}</h2>
-            <div className="card__container card--set card--setscreen"></div>
+            <form className="set__form">
+                <div className="card__container card--set card--setscreen"></div>
+
+                <div className="form__group">
+                    <label htmlFor="title" className="details__title" size="46">title</label>
+                    <input type="text" name="title" className="details__description details--set"/>
+
+                    <label htmlFor="description" className="details__title">description</label>
+                    <textarea type="text" name="title" className="details__description details--set" rows="5" col="45"></textarea>
+                </div>
+
+                <input type="text" name="title" placeholder="search a collection" className="details__description details--set"/>
+            </form>
         </div>
 
     )
