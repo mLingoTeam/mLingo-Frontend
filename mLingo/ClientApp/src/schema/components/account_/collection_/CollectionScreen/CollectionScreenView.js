88ab89c3b457752ac4f5b35b6e651263c65ab422
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2021-03-11 08:56:41 +0100

    implement collection panel

--- mLingo/ClientApp/src/schema/components/account_/collection_/CollectionScreen/CollectionScreenView.js
+++ mLingo/ClientApp/src/schema/components/account_/collection_/CollectionScreen/CollectionScreenView.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import CollectionFlashcard from '../CollectionFlashcard/CollectionFlashcard';
 import Loading from '../../../loading/Loading';
+import { FaBook, FaTrashAlt, FaPencilAlt, FaRegFolderOpen, FaRegClone} from 'react-icons/fa';
+import Chart from "./charts/Chart.jsx";
 
 const CollectionScreenView = ( { state, functions } ) => {
 
@@ -11,10 +13,30 @@ const CollectionScreenView = ( { state, functions } ) => {
             {
                 state.loaded ? state.collection ?
                 <div>
-                    <div className="collection__info">
+                    <div className="collection__center">
+                    {
+                        state.collection ? <div className="flex--around collection__options">
+                            <button className="collection__button"> <FaBook/> Learn</button>
+                            <Link className="collection__button" onClick={functions.editCollection} to="/create" > <FaPencilAlt/> Edit</Link>
+                            <button className="collection__button" onClick={functions.removeCollection}><FaTrashAlt/>Remove</button>
+                            <button className="collection__button"><FaRegFolderOpen/>Add to set</button>
+                        </div> : null
+                    }
+                    <div class="collection__info">
+                        <div className=""></div>
                         <div className="collection__title">{state.collection.name}</div>
                         <div className="collection__description">{state.collection.description}</div>
+                        <div className="flex--between">
+                            <Chart/>
+                            <div className="collection__total">
+                                <FaRegClone/>
+                                <div className="collection__revised">598 revised</div>
+                            </div>
+                        </div>
                     </div>
+
+                    </div>
+
                     {
                         state.collection.cards.map((element, index) => (
                             <CollectionFlashcard set={element} index={index}/> ))
@@ -23,12 +45,7 @@ const CollectionScreenView = ( { state, functions } ) => {
                 </div> : <h1>Collection removed!</h1> : <Loading/>
             }
                 <Link to='/head' className="green--button">go back</Link>
-            {
-                state.collection ? <div className="flex--around">
-                    <button className="green--button" onClick={functions.removeCollection}>Remove Collection</button>
-                    <Link className="green--button" onClick={functions.editCollection} to="/create" >Edit Collection</Link>
-                </div> : null
-            }
+
         </div>
 
     )
